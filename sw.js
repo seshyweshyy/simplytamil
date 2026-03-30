622b3ae22020ff4948e5b54b8981ed58091cd09d
seshyweshyy <seshanengunan1@gmail.com>
2026-03-31 08:13:42 +1100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,2 @@
 self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => self.clients.claim());
-
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
-});
+self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
